a1c9297768ed2029ed079d6187e3a3e0b65061f1
Jhonatan Henkel <jhowrf@gmail.com>
2024-04-23 07:49:35 -0300

    build: import spring boot base project.

---
+++ src/main/java/com/personapiv2/demo/PersonApiV2Application.java
@@ -0,0 +1,13 @@
+package com.personapiv2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiV2Application.class, args);
+	}
+
+}
